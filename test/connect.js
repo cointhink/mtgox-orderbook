58a6639bd4a8cf89e9ea7b038b718a8e053f3c93
Don Park <don@donpark.org>
2013-04-22 14:20:24 -0700

    depth message test

--- test/connect.js
+++ test/connect.js
@@ -11,12 +11,43 @@ var mocketio = nodemock.mock("connect").takes(mtgox.socket_url).returns(mockio)
 vows.describe('mtgox-orderbook').addBatch({
     'connect to mtgox': {
       topic: function(){
-        mtgox.on('connect', this.callback)
+        var that = this
+        mtgox.on('connect', function(payload){
+          that.callback(null, payload)
+        })
         mtgox.connect(mocketio, 'usd')
         mockio.emit('connect')
       },
-      'connection succeeds': function (err) {
-        assert.isUndefined(err);
+      'connection succeeds': function () {
+        assert.isTrue(true);
       },
+    },
+    'depth message':{
+      topic: function(){
+        var that = this
+        mtgox.on('depth', function(payload){
+          that.callback(null, payload)
+        })
+        mockio.emit('message',
+          { channel: '24e67e0d-1cad-4cc0-9e7a-f8523ef460fe',
+            channel_name: 'depth.BTCUSD',
+            op: 'private',
+            origin: 'broadcast',
+            private: 'depth',
+            depth:
+             { price: '116.92',
+               type: 2,
+               type_str: 'bid',
+               volume: '0',
+               price_int: '11692000',
+               volume_int: '0',
+               item: 'BTC',
+               currency: 'USD',
+               now: '1366664736649566',
+               total_volume_int: '199613915' } })
+      },
+      'signal': function (depth) {
+        assert.equal(depth.price, '116.92')
+      }
     }
 }).export(module)
